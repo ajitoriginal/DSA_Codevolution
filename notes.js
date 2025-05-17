a9b672f6f62f847f6b62acafc3028c536eb2aaff
Ajit Singh <ajitsinghoriginal@gmail.com>
2025-05-17 22:55:40 +0530

    first commit

---
+++ notes.js
@@ -0,0 +1,56 @@
+/* 
+DEFINITION
+--A data structure is way to store and organize data
+so that it can be used efficiently
+--A data structure is a collection of data values, the relationships
+among them, and the functions or operations that can be applied to that data
+
+
+WHY LEARN DATA STRUCTURES?
+--Almost every application we build involves data that is
+modelled in a certain way
+--To efficiently manage the data, you need data structures
+--Imagine walking into a library only to find out there is no efficient
+organization of the books and they are stored randomly
+--The difference between a function taking a few milliseconds vs a few seconds
+or even minutes comes down to the selection of the right data structure
+--Data structures help you solve problems in a more efficient way, both in
+terms of time and memory
+--Learning about data structures also help you gain a more profound understanding
+of things you're already aware of
+--DOM - Tree data structure
+--Browser back and forward - Stack data structure
+--OS job scheduling - Queue data structure
+
+OVERVIEW
+--Built in data structures
+Arrays
+Objects
+Sets
+Maps
+--Custom data structures
+Stacks
+Queues
+Circular Queues
+Linked Lists
+Hash Tables
+Trees
+Graphs
+
+ARRAY
+--An array is a data structure that can hold a collection of values
+--Arrays can contain a mix of different data types. You can store strings, booleans, numbers
+or even objects all in the same array
+--Arrays are resizable. You don't have to declare the size of an array before creating it
+--JavaScript arrays are zero-indexed and the insertion order is maintained
+--Arrays are iterables. They can be used with a for of loop
+
+ARRAY - BIG-O TIME COMPLEXITY
+--Insert/Remove from end - O(1)
+--Insert/Remove from beginning - O(n)
+--Access - O(1)
+--Search - O(n)
+--Push/Pop - O(1)
+--Shift/unshift/concat/slice/splice - O(n)
+--forEach/map/filter/reduce - O(n)
+*/
