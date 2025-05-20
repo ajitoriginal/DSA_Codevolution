a5fca6ba4b5de40a72993bfa14a983e1d72b457e
Ajit Singh <ajitsinghoriginal@gmail.com>
2025-05-20 13:45:15 +0530

    implemented print method in linked list

--- linkedList.js
+++ linkedList.js
@@ -18,9 +18,39 @@ class LinkedList {
     getSize() {
         return this.size
     }
+
+    prepend(value) {
+        const node = new Node(value)
+        if(!this.isEmpty()) {
+            node.next = this.head  
+        }
+        this.head = node
+        this.size++
+    }
+
+    print() {
+        if(this.isEmpty()) {
+            console.log('List is empty')
+        } else {
+            let result = ''
+            let current = this.head
+            while(current) {
+                result += current.value + ' '
+                current = current.next
+            }
+            console.log('List: '+ result)
+        }
+    }
 }
 
 
 const list = new LinkedList()
 console.log(`isEmpty: ${list.isEmpty()}`)
 console.log(`size: ${list.getSize()}`)
+list.print()
+list.prepend(10)
+list.prepend(20)
+list.prepend(30)
+list.prepend(40)
+console.log(`size: ${list.getSize()}`)
+list.print()
